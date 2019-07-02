f8467ee9f128bb8f4dbdd61631d124b783a9ddfe
Jonathan.Cady <jonathan.cady@Ticketmaster.com>
2019-07-02 14:13:10 -0700

    basic arch made

--- seahacks/src/App.js
+++ seahacks/src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Row, Col, Container } from 'reactstrap';
+import { Header, Heading} from '@ticketmaster/aurora'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Seahacks</p>
-      </header>
+      <Header
+          withSpotLight
+      >
+        <Heading level={1}>
+          <Heading.Strong>Explore</Heading.Strong>
+          {/*{" "}*/}
+          {/*<Heading.Span>Header</Heading.Span>*/}
+        </Heading>
+      </Header>
+      <Container>
+        <Row style={{ border: '2px solid black' }}>
+          Filter
+        </Row>
+        <Row>
+          <Col xs={8} style={{ border: '2px solid black' }}>
+            Map
+          </Col>
+          <Col xs={4} style={{ border: '2px solid black' }}>
+            Scroll
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
