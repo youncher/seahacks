8dfd2abba902f83de58a427101e5dc7ada731344
Verena.Nicolaou <verena.nicolaou@ticketmaster.com>
2019-07-02 22:25:03 -0700

    links

--- seahacks/src/components/EventsDisplay.js
+++ seahacks/src/components/EventsDisplay.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
 import SpotifyPreview from "../SpotifyPreview";
-import {Row} from '@ticketmaster/aurora';
+import ListRow from '@ticketmaster/aurora';
+import {Row} from "reactstrap";
+
 
 export default class EventsDisplay extends Component {
 
-    render() {
+  render() {
 
-        return (
-            <div>
-                <Row style={{ height: '70vh', overflowY: 'auto' }}>
-                    {this.props.selectedEvents.map(event => {
-                        return <div>
-                            <br />{event.name}</div>
-                    })}
-                </Row>
-                <Row style={{ height: '30vh' }}>
-                    {/* fill box */}
-                    <SpotifyPreview artistName={"Adele"} />
-                </Row>
-            </div>
-        );
-    }
+    return (
+        <div>
+          <Row style={{ height: '70vh', overflowY: 'auto' }}>
+            {this.props.selectedEvents.map(event => {
+              return <div>
+                <br />
+                <a className='independent-link' href={event.url}>{event.name}</a>
+              </div>
+            })}
+          </Row>
+          <Row style={{ height: '30vh' }}>
+            {/* fill box */}
+            <SpotifyPreview artistName={"Adele"} />
+          </Row>
+        </div>
+    );
+  }
 }
